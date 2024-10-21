edda62b0f55b37b396f6bc219e13913e0e0782d3
Todd Dowty <todd.dowty@energyresearchlab.com>
2024-10-21 15:04:49 +0100

    Washing up

--- S1084-AutoRelease/Main.cs
+++ S1084-AutoRelease/Main.cs
@@ -92,33 +92,6 @@ namespace S1084_AutoRelease
             if (SoftwareProjects.GetElementsByTagName(SubProjectListComboBox.Text)[0] != null)
             {
                 EditSubProject edit = new EditSubProject(db, SubProjectListComboBox.Text);
-
-                //AddSubProject Sxxxx = new AddSubProject(SubProjectListComboBox.Text);
-                //XmlNode node = SoftwareProjects.GetElementsByTagName(SubProjectListComboBox.Text)[0];
-                //Sxxxx.number = node.Name;
-                //Sxxxx.shortName = node.Attributes["shortName"].Value;
-                //Sxxxx.platform = node.Attributes["platform"].Value;
-                //Sxxxx.outputType = node.Attributes["outputType"].Value;
-                //Sxxxx.outputPath = node.Attributes["outputPath"].Value;
-                //Sxxxx.versionPath = node.Attributes["versionPath"].Value;
-                //Sxxxx.releasesPath = node.Attributes["releasesPath"].Value;
-                //Sxxxx.archivePath = node.Attributes["archivePath"].Value;
-                //Sxxxx.description = node.InnerText;
-                //Sxxxx.Refresh();
-
-                //var result = Sxxxx.ShowDialog();
-                //if (result == DialogResult.OK)
-                //{
-                //    node.Attributes["shortName"].Value = Sxxxx.shortName;
-                //    node.Attributes["platform"].Value = Sxxxx.platform;
-                //    node.Attributes["outputType"].Value = Sxxxx.outputType;
-                //    node.Attributes["outputPath"].Value = Sxxxx.outputPath;
-                //    node.Attributes["versionPath"].Value = Sxxxx.versionPath;
-                //    node.Attributes["releasesPath"].Value = Sxxxx.releasesPath;
-                //    node.Attributes["archivePath"].Value = Sxxxx.archivePath;
-                //    node.InnerText = Sxxxx.description;
-                //    db.Save(db.DocumentElement.GetAttribute("path"));
-                //}
             }
         }
 
