6c9443136ac37eeb1a86c7e030cfc5a3a60cba5b
Todd Dowty <todd.dowty@energyresearchlab.com>
2024-10-22 15:14:43 +0100

    Telling user to build all S/W. Again, in future, hope to make this step automated

--- S1084-AutoRelease/NormalRelease.cs
+++ S1084-AutoRelease/NormalRelease.cs
@@ -13,6 +13,7 @@ using System.Xml;
 using System.Management.Automation;
 using System.IO;
 using System.Collections.ObjectModel;
+using System.Security.Policy;
 
 
 namespace S1084_AutoRelease
@@ -95,16 +96,26 @@ namespace S1084_AutoRelease
 
         private void OkayButton_Click(object sender, EventArgs e)
         {
-           /* XmlElement newRelease = db.CreateElement(version);
-            newRelease.SetAttribute("unplanned", BacklogTextBox.Text);
-            newRelease.SetAttribute("todo", ToDoTextBox.Text);
-            newRelease.SetAttribute("inProgress", InProgressTextBox.Text);
-            newRelease.SetAttribute("done", DoneTextBox.Text);
-            sprints.AppendChild(newRelease);
-            db.Save(db.DocumentElement.GetAttribute("path"));
+            this.DialogResult = DialogResult.Abort;
 
-            GenerateReport gen = new GenerateReport(db, projectName); */
+            /* XmlElement newRelease = db.CreateElement(version);
+             newRelease.SetAttribute("unplanned", BacklogTextBox.Text);
+             newRelease.SetAttribute("todo", ToDoTextBox.Text);
+             newRelease.SetAttribute("inProgress", InProgressTextBox.Text);
+             newRelease.SetAttribute("done", DoneTextBox.Text);
+             sprints.AppendChild(newRelease);
+             db.Save(db.DocumentElement.GetAttribute("path"));
 
+             GenerateReport gen = new GenerateReport(db, projectName); */
+
+            XmlNode software = project.GetElementsByTagName("Software")[0];
+            if (software == null)
+            {
+                MessageBox.Show("Release process aborted: There is no software included in " + projectName);
+                this.Close();
+            }
+
+            // STEP 1 - Check out Develop Branch and make sure it is up to date
             string repoPath = db.GetElementsByTagName(projectName)[0].Attributes["repoPath"].Value;
 
             using (PowerShell powershell = PowerShell.Create())
@@ -114,7 +125,54 @@ namespace S1084_AutoRelease
                 powershell.AddScript(@"git checkout Develop");
 
                 Collection<PSObject> results = powershell.Invoke();
+
+                string[] response = results[0].BaseObject.ToString().Split("'");
+
+                if (response[0] == "Your branch is behind ")
+                {
+                    powershell.AddScript(@"git pull");
+                    powershell.Invoke();
+                    this.DialogResult = DialogResult.OK;
+                }
+                else if (response[0] == "Your branch is up to date with ")
+                {
+                    this.DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    MessageBox.Show("Release process aborted:\n\n" + response[0]);
+                }
+
+                // STEP 2 - Tag Develop Branch with latest release version
+                // Only continue if fetching and updating Develop branch was successful
+                if (this.DialogResult == DialogResult.OK)
+                {
+                    powershell.AddScript($"git tag {version}");
+                    powershell.AddScript(@"git push origin --tags");
+                    results = powershell.Invoke();
+
+                    // STEP 3 - Tell user to build: Would be much nicer if we could auto build.
+                    string message = "WARNING: ONLY Click OK AFTER building ALL software!\n\nPlease Build/Compile:\n\n";
+                    foreach (XmlNode Sxxxx in software)
+                    {
+                        message += Sxxxx.Name;
+                        message += "\n";
+                    }
+                    //all software and Press Enter when done..."
+
+                    if (MessageBox.Show(message, "Build", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                    {
+                        MessageBox.Show("Release process aborted: User chose not to build software");
+                    }
+                    else
+                    {
+                        // Here we have to assume user has built all software as requested
+                    }
+                     
+                }
             }
+
+
 
 
             this.Close();
