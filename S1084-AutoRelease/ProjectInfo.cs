b3f1c9ad0480b058f3a3bc929defdbe3ad9937a3
Todd Dowty <todd.dowty@energyresearchlab.com>
2024-10-28 16:50:57 +0000

    Washing up

--- S1084-AutoRelease/ProjectInfo.cs
+++ S1084-AutoRelease/ProjectInfo.cs
@@ -55,7 +55,7 @@ namespace S1084_AutoRelease
             checkColumn.HeaderText = "Included in Build";
             checkColumn.Width = 50;
             checkColumn.ReadOnly = false;
-            checkColumn.FillWeight = 10; //if the datagridview is resized (on form resize) the checkbox won't take up too much; value is relative to the other columns' fill values
+            checkColumn.FillWeight = 10;
             TableOfSxxxx.Columns.Add(checkColumn);
 
             if (name != "") // If editing an existing project then:
@@ -73,27 +73,12 @@ namespace S1084_AutoRelease
                 {
                     foreach (XmlNode node in Software)
                     {
-                        //Sxxxx sxxxx = new Sxxxx();
-                        //sxxxx.name = node.Name;
-                        //sxxxx.included = node.Attributes["included"].Value;
-                        //subProjects.Add(sxxxx);
-
                         if (node.Attributes["included"].Value == "yes")
                             TableOfSxxxx.Rows.Add(node.Name, GetSxxxxAttributeFromName(node.Name, "shortName"), GetSxxxxAttributeFromName(node.Name, "platform"), true);
                         else
                             TableOfSxxxx.Rows.Add(node.Name, GetSxxxxAttributeFromName(node.Name, "shortName"), GetSxxxxAttributeFromName(node.Name, "platform"), false);
                     }
-
-                    //subProjects = subProjects.OrderBy(o => o.name).ToList();
-
-                    //foreach (Sxxxx subProject in subProjects)
-                    //{
-                    //    AddSxxxxProductToTable(subProject);
-                    //}
                 }
-
-                //AddFinalRowToSxxxxTable();
-
 
                 XmlElement sprints = (XmlElement)project.GetElementsByTagName("Sprints")[0];
 
@@ -160,84 +145,6 @@ namespace S1084_AutoRelease
             return retVal;
         }
 
-        /*private void AddSxxxxProductToTable(Sxxxx sxxxx)
-        {
-            Button subProjectButton = new Button();
-            subProjectButton.BackColor = Color.FromArgb(243, 111, 247);
-            subProjectButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            subProjectButton.Location = new Point(subProjectButton_x, subProjectButton_y);
-            subProjectButton.Name = "subProjectButton";
-            subProjectButton.Size = new Size(80, 50);
-            subProjectButton.TabIndex = 0;
-            subProjectButton.Text = sxxxx.name;
-            subProjectButton.UseVisualStyleBackColor = false;
-            subProjectButton.Click += OpenSubProjectButton_Click;
-
-            Button removeSubProjectButton = new Button();
-            removeSubProjectButton.Name = "Remove" + sxxxx.name;
-            removeSubProjectButton.Size = new Size(50, 50);
-            removeSubProjectButton.Image = Image.FromFile("C:\\Projects\\Windows Apps\\S1084-AutoRelease\\Icons\\Remove-50x50.png");
-            removeSubProjectButton.Click += RemoveSubProjectButton_Click;
-
-            int width = TableLayoutPanel.Size.Width;
-            int height = TableLayoutPanel.Size.Height;
-
-            int row = TableLayoutPanel.RowCount;
-            TableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            TableLayoutPanel.RowCount++;
-
-            TableLayoutPanel.Controls.Add(removeSubProjectButton, 0, row);
-            TableLayoutPanel.Controls.Add(subProjectButton, 1, row);
-
-            height += 50;
-            TableLayoutPanel.Size = new Size(width, height);
-
-            string shortDescription = "";
-            foreach (XmlNode softwareProject in db.GetElementsByTagName("SoftwareProjects")[0].ChildNodes)
-            {
-                if (sxxxx.name == softwareProject.Name)
-                {
-                    Label desc = new Label();
-                    desc.AutoSize = true;
-                    desc.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-                    desc.Text = softwareProject.Attributes["shortName"].Value;
-                    shortDescription = softwareProject.Attributes["shortName"].Value;
-                    TableLayoutPanel.Controls.Add(desc, 2, row);
-                    break;
-                }
-            }
-
-            CheckBox incl = new CheckBox();
-            incl.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-
-            if (sxxxx.included == "yes")
-                incl.Checked = true;
-            else
-                incl.Checked = false;
-
-            TableLayoutPanel.Controls.Add(incl, 3, row);
-        }
-
-        private void AddFinalRowToSxxxxTable()
-        {
-            Button addSubProjectButton = new Button();
-            addSubProjectButton.Size = new Size(50, 50);
-            addSubProjectButton.Image = Image.FromFile("C:\\Projects\\Windows Apps\\S1084-AutoRelease\\Icons\\Add-50x50.png");  //(Image)resources.GetObject("AddSubProjectButton.Image");
-            addSubProjectButton.Click += AddSubProjectButton_Click;
-
-            int width = TableLayoutPanel.Size.Width;
-            int height = TableLayoutPanel.Size.Height;
-
-            int row = TableLayoutPanel.RowCount;
-            TableLayoutPanel.RowCount++;
-            TableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            TableLayoutPanel.Controls.Add(addSubProjectButton, 0, row);
-
-            height += 50;
-            TableLayoutPanel.Size = new Size(width, height);
-        }*/
-
-
 
         private bool SaveProject()
         {
@@ -269,45 +176,38 @@ namespace S1084_AutoRelease
             project.SetAttribute("stage", StageComboBox.Text);
             project.SetAttribute("status", StatusComboBox.Text);
 
-            //if (subProjects.Count > 0)
-            //if (TableOfSxxxx.Rows.Count > 0)
-            //{
-                XmlElement software = db.CreateElement("Software");
-                foreach (DataGridViewRow row in TableOfSxxxx.Rows)
+            XmlElement software = db.CreateElement("Software");
+            foreach (DataGridViewRow row in TableOfSxxxx.Rows)
+            {
+                if (row.Cells[0].Value != null)
                 {
-                    if (row.Cells[0].Value != null)
-                    {
-                        XmlElement sub = db.CreateElement(row.Cells[0].Value.ToString());
+                    XmlElement sub = db.CreateElement(row.Cells[0].Value.ToString());
 
-                        string inc = "no";
-                        if ((bool)row.Cells[3].Value)
-                            inc = "yes";
+                    string inc = "no";
+                    if ((bool)row.Cells[3].Value)
+                        inc = "yes";
 
-                        sub.SetAttribute("included", inc);
-                        software.AppendChild(sub);
-                    }
+                    sub.SetAttribute("included", inc);
+                    software.AppendChild(sub);
                 }
-                project.AppendChild(software);
-            //}
+            }
+            project.AppendChild(software);
 
 
-            //if (endOfSprints.Count > 0)
-            //{
-                XmlElement sprints = db.CreateElement("Sprints");
+            XmlElement sprints = db.CreateElement("Sprints");
 
-                foreach (endOfSprint sprint in endOfSprints)
-                {
-                    XmlElement xmlSprint = db.CreateElement(sprint.name);
-                    xmlSprint.SetAttribute("date", sprint.date);
-                    xmlSprint.SetAttribute("unplanned", sprint.unplanned);
-                    xmlSprint.SetAttribute("todo", sprint.todo);
-                    xmlSprint.SetAttribute("inProgress", sprint.inProgress);
-                    xmlSprint.SetAttribute("done", sprint.done);
-                    sprints.AppendChild(xmlSprint);
-                }
+            foreach (endOfSprint sprint in endOfSprints)
+            {
+                XmlElement xmlSprint = db.CreateElement(sprint.name);
+                xmlSprint.SetAttribute("date", sprint.date);
+                xmlSprint.SetAttribute("unplanned", sprint.unplanned);
+                xmlSprint.SetAttribute("todo", sprint.todo);
+                xmlSprint.SetAttribute("inProgress", sprint.inProgress);
+                xmlSprint.SetAttribute("done", sprint.done);
+                sprints.AppendChild(xmlSprint);
+            }
 
-                project.AppendChild(sprints);
-            //}
+            project.AppendChild(sprints);
 
             db.GetElementsByTagName("Projects")[0].AppendChild(project);
             db.Save(db.DocumentElement.GetAttribute("path"));
@@ -334,71 +234,6 @@ namespace S1084_AutoRelease
             }
         }
 
-
-
-        /*private void AddSubProjectButton_Click(object sender, EventArgs e)
-        {
-            SelectSubProject SxxxxForm = new SelectSubProject(db);
-            var result = SxxxxForm.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                foreach (Sxxxx subProject in subProjects)
-                {
-                    if (subProject.name == SxxxxForm.selectedSubProject)
-                    {
-                        MessageBox.Show(SxxxxForm.selectedSubProject + " is already included in project " + ProjectNameTextBox.Text);
-                        return;
-                    }
-                }
-
-                Sxxxx sxxxx = new Sxxxx();
-                sxxxx.name = SxxxxForm.selectedSubProject;
-                sxxxx.included = "no";
-                subProjects.Add(sxxxx);
-                subProjects = subProjects.OrderBy(o => o.name).ToList();
-
-                TableLayoutPanel.SuspendLayout();
-                TableLayoutHelper.RemoveArbitraryRow(TableLayoutPanel, TableLayoutPanel.RowCount - 1); // Remove last row. Will re-add after
-                AddSxxxxProductToTable(sxxxx);
-                AddFinalRowToSxxxxTable();
-                TableLayoutPanel.ResumeLayout();
-            }
-        }
-        private void RemoveSubProjectButton_Click(object sender, EventArgs e)
-        {
-            Button removeButton = (Button)sender;
-            Control[] SxxxxButtons = TableLayoutPanel.Controls.Find("subProjectButton", true);
-            Sxxxx sxxxx = new Sxxxx();
-
-            for (int rowIndex = 0; rowIndex < TableLayoutPanel.RowCount; rowIndex++)
-            {
-                sxxxx = subProjects[rowIndex];
-
-                if (("Remove" + SxxxxButtons[rowIndex].Text) == removeButton.Name)
-                {
-                    TableLayoutPanel.SuspendLayout();
-                    TableLayoutHelper.RemoveArbitraryRow(TableLayoutPanel, rowIndex + 1);
-                    TableLayoutPanel.ResumeLayout();
-
-                    subProjects.Remove(sxxxx);
-                    break;
-                }
-            }
-        }
-
-        private void OpenSubProjectButton_Click(object sender, EventArgs e)
-        {
-            Button subProjectButton = (Button)sender;
-
-            foreach (Sxxxx sxxxx in subProjects)
-            {
-                if (sxxxx.name == subProjectButton.Text)
-                {
-                    EditSubProject edit = new EditSubProject(db, sxxxx.name);
-                    this.Close();
-                }
-            }
-        }*/
 
         private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
